105df1a72e0f09942f9bafc94b7723f349a5256f
jumail12 <user.jumailjumi2003@gmail.com>
2025-11-19 00:27:24 +0530

    user netity role netity

--- ChatAppSlnVersionII.Domain/Entities/Roles/Role.cs
+++ ChatAppSlnVersionII.Domain/Entities/Roles/Role.cs
@@ -22,19 +22,19 @@ namespace ChatAppSlnVersionII.Domain.Entities.Roles
         public DateTime? role_DeletedAt { get; set; }
     }
 
-    public class RoleMapping
+    public class role_mapping
     {
         public string? map_id { get; set; }
-        public string? map_userId { get; set; }
-        public string? map_roleGrpId { get; set; }
+        public string? map_userid { get; set; }
+        public string? map_role_grpid { get; set; }
         public bool? map_isdeleted { get; set; }
 
-        public string? map_CreatedBy { get; set; }
-        public string? map_ModifiedBy { get; set; }
-        public string? map_DeletedBy { get; set; }
+        public string? map_createdby { get; set; }
+        public string? map_modifiedby { get; set; }
+        public string? map_deletedby { get; set; }
 
-        public DateTime map_CreatedAt { get; set; }
-        public DateTime? map_ModifiedAt { get; set; }
-        public DateTime? map_DeletedAt { get; set; }
+        public DateTime map_createdat { get; set; }
+        public DateTime? map_modifiedat { get; set; }
+        public DateTime? map_deletedat { get; set; }
     }
 }
---
+++ ChatAppSlnVersionII.Domain/Entities/Users/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppSlnVersionII.Domain.Entities.Users
+{
+    public class user_header
+    {
+        public string ? h_user_id { get; set; }
+        public string ? h_user_name { get; set; }
+        public string ? h_user_email { get; set; }
+        public string ? h_user_password { get; set; }
+        public bool? h_is_active { get; set; }
+        public bool ? h_is_deleted { get; set; }
+        public DateTime h_last_login { get; set; }
+        public string ? h_user_createdby { get; set; }
+        public DateTime h_user_createdat { get; set; }
+        public string ? h_user_modifiedby { get; set; }
+        public DateTime ? h_user_modifiedat { get; set; }
+        public string ? h_user_deletedby { get; set; }
+        public DateTime ? h_user_deletedat { get; set; }
+    }
+
+    public class user_details 
+    {
+        public string ? d_user_id { get; set; }
+        public string? dh_user_id { get; set; }
+        public string? d_user_name { get; set; }
+        public string? d_avatarurl { get; set; }
+        public string? d_bio { get; set; }
+        public bool? d_is_deleted { get; set; }
+        public string? d_user_createdby { get; set; }
+        public DateTime d_user_createdat { get; set; }
+        public string? d_user_modifiedby { get; set; }
+        public DateTime? d_user_modifiedat { get; set; }
+        public string? d_user_deletedby { get; set; }
+        public DateTime? d_user_deletedat { get; set; }
+    }
+
+}
